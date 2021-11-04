f0d555fe50f7f9c60592629d32290f8ae56adc11
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-03 23:06:38 -0400

    update-config

--- src/firebase/credenciales.js
+++ src/firebase/credenciales.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Añade aquí tus credenciales
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  storageBucket: "...",
-  messagingSenderId: "...",
-  appId: "...",
+  apiKey: "AIzaSyCnFuFDC7vPmVFHIJddSnwptKU60FfR32c",
+  authDomain: "discord-chat-e157f.firebaseapp.com",
+  projectId: "discord-chat-e157f",
+  storageBucket: "discord-chat-e157f.appspot.com",
+  messagingSenderId: "908445997251",
+  appId: "1:908445997251:web:493659e4727ee2a8d86a76"
 };
 
 // Inicializamos la aplicación y la guardamos en firebaseApp
