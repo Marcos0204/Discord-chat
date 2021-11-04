61c71bb29a77fa875bb851b36e64d36baca7c91f
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-04 09:51:40 -0400

    structure-inital-login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Button } from '@material-ui/core' 
 
 const Login = () => {
+    function loginWithGmail() {
+
+    }
     return (
-        <div>
-            <h1>login</h1>
+        <div className='login'>
+            <div className='Login__logo'>
+                <img src="https://picsum.photos/420" alt="" />
+            </div>
+            <Button onClik={loginWithGmail}>Acceder con Google</Button>
         </div>
     )
 }
