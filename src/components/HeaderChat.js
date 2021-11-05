3dbf7b53281e6fdd8a4f3a6a26cc4b0a355c145e
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-11-05 18:17:51 -0400

    header_chat

---
+++ src/components/HeaderChat.js
@@ -0,0 +1,38 @@
+import React from "react";
+import {
+    Notifications,
+    Room,
+    PeopleAlt,
+    Search,
+    Send,
+    Help,
+  } from "@material-ui/icons";
+
+const HeaderChat = ({nameChannel:''}) => {
+    return (
+        <div className="chatHeader">
+      <div className="chatHeader__left">
+        <h3>
+          <span className="chatHeader__hash"> #</span>
+          {nameChannel}
+        </h3>
+      </div>
+
+      <div className="chatHeader__right">
+        <Notifications />
+        <Room />
+        <PeopleAlt />
+
+        <div className="chatHeader__search">
+          <input placeholder="buscar" />
+          <Search />
+        </div>
+
+        <Send />
+        <Help />
+      </div>
+    </div>
+    )
+}
+
+export default HeaderChat
